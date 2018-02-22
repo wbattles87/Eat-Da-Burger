c8bfc5d5b6788102fbfe3941b08a655564d681de
Will <wbattles87@yahoo.com>
2018-02-22 17:18:16 -0600

    more updated code almost working

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Bleu Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Sliders', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avacado Burger', true);
