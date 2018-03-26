b68e18ce82356f801dc783c74168e934be96faf8
Will <wbattles87@yahoo.com>
2018-03-26 17:45:23 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function () {
-    console.log("Listening on port:%s", PORT);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
