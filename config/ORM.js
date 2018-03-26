afe44739921903147883baa8b713487aad0cd834
Will <wbattles87@yahoo.com>
2018-03-26 18:06:46 -0500

    heroku

--- config/ORM.js
+++ config/ORM.js
@@ -1,7 +1,5 @@
-
 // Here is the O.R.M. where you write functions that takes inputs and conditions
 // and turns them into database commands like SQL.
-
 var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
